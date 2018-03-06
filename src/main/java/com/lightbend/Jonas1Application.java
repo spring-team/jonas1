4ccc14c0c8e87ca5895643bb62d5c618484fbd78
Rod Johnson <johnsonroda@gmail.com>
2018-03-06 16:39:08 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/lightbend/Jonas1Application.java
@@ -0,0 +1,12 @@
+package com.lightbend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jonas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jonas1Application.class, args);
+	}
+}
---
+++ src/test/java/com/lightbend/Jonas1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lightbend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jonas1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
